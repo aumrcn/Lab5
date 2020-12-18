1a2eb74e23703548587135136838036eb93e9f48
aumrcn <aumkung20728@gmail.com>
2020-12-18 13:32:25 +0700

    Kuayba

---
+++ exercise5_2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cmath>
+#include<string>
+using namespace std;
+
+int main(){
+    string text1 = "";
+    string text2 = "";
+    int N;
+    int count = 0;
+    
+    cout << "Enter the first text: ";
+    cin  >> text1;
+    cout << "\nEnter the second text: ";
+    cin  >> text2;
+    cout << "\nEnter N: ";
+    cin  >> N;
+        while(count < N){
+            if(count%2 == 0){
+                cout << text1 << " ";
+            }else{
+                cout << text2 << " ";
+            }
+            count++;
+        }
+    return 0;    
+}
